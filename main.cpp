0427241dbf990be2926b68ac302320e6cff12ba2
Elias Daniel Bromerschenkel <>
2021-05-22 19:02:16 +0000

    int main

--- main.cpp
+++ main.cpp
@@ -6,3 +6,16 @@ char msg[MAX_SIZE];
 char nova_msg[MAX_SIZE];
 
 int encode(char *mensagem, char from_char, char to_char, char *nova_msg);
+
+int main() {
+	char from_char;
+	char to_char;
+	int n_trocas;
+
+	cout << "Digite uma palavra: ";
+	cin >> msg;
+	cout << "Caractere a ser substituido: ";
+	cin >> from_char;
+	cout << "Caractere pelo qual deve ser substituido: ";
+	cin >> to_char;
+}
