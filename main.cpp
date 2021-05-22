efab9ce6c948666e27b1a3a4d0f1c6d06d76b591
Elias Daniel Bromerschenkel <>
2021-05-22 18:59:48 +0000

    declaracoes

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+#define MAX_SIZE 255
+char msg[MAX_SIZE];
+char nova_msg[MAX_SIZE];
+
+int encode(char *mensagem, char from_char, char to_char, char *nova_msg);
