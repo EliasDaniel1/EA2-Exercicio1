8cbdb9a0c080a1901185189503c756e6605fc012
Elias Daniel Bromerschenkel <>
2021-05-22 19:08:59 +0000

    Final

--- main.cpp
+++ main.cpp
@@ -18,4 +18,25 @@ int main() {
 	cin >> from_char;
 	cout << "Caractere pelo qual deve ser substituido: ";
 	cin >> to_char;
+
+  n_trocas = encode(msg, from_char, to_char, nova_msg);
+  cout << nova_msg << endl;
+	cout << msg << endl;
+	cout << n_trocas << endl;
+}
+
+int encode(char *menssagem, char from_char, char to_char, char *nova_msg){
+	int counter = 0;
+	int msg_index = 0;
+	while(menssagem[msg_index] != '\0'){
+		if(menssagem[msg_index] == from_char){
+			nova_msg[msg_index] = to_char;
+			counter++;
+		}
+		else{
+			nova_msg[msg_index] = menssagem[msg_index];
+		}
+		msg_index++;
+	}
+	return counter;
 }
